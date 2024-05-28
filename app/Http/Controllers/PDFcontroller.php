b7c914bb91ee1548323715dd205f27e86c4710c8
Kevin Pacheco <david3797@hotmail.com>
2024-05-28 08:30:39 -0500

    descripicion pdf

--- app/Http/Controllers/PDFcontroller.php
+++ app/Http/Controllers/PDFcontroller.php
@@ -15,9 +15,8 @@ class PDFcontroller extends Controller
     public function animalreport($id)
     {
         $animal = Animales::join('especies', 'especies.id', '=', 'animales.especie_id')->join('razas','razas.id','=','animales.raza_id')->where('animales.id', $id)->first();
-        $cuidados =Cuidados::join('animales', 'animales.id', '=', 'cuidados.animal_id')
-            ->where('check', '=', true)
-            ->where('animales.id', $id)->get();
+        $cuidados =Cuidados::where('check', '=', true)->where('cuidados.animal_id', $id)->get();
+
         $fecha = Carbon::now()->toDateString();
 
         $pdf = Pdf::loadView('pdf.animalreport', compact('animal','cuidados'));
